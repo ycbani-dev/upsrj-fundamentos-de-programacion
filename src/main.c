4157b69824b0837e178cf8ff3207ca37a94b7255
ycbani-dev <soy.criszzz1070@gmail.com>
2026-01-24 14:44:19 -0600

    Determinar si un número es par

--- src/main.c
+++ src/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
+// 1. Declaración de la función
+int is_even(int num);
+
+int main() {
+    int num; // Declaración de la variable local
+
+    printf("Ingrese un numero: ");
+    // Usamos %i para leer el entero
+    scanf("%i", &num);
+
+    if (is_even(num)) {
+        printf("El numero es par\n");
+    } 
+    else {
+        printf("El numero es impar\n");
+    }
+
     return 0;
-} 
+}
+
+int is_even(int num) {
+    if (num % 2 == 0) {
+        return 1;
+    } 
+    else {
+        return 0;
+    }
+}
